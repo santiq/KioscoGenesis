4836926dcfaf2b501e8582988902afbcab7a066f
MatiasHeredia <r0bin24xp@gmail.com>
2019-01-05 20:26:30 -0300

    cambios en el servidor agregado Firebase

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 var express = require('express');
 var app = express();
 var path = require ('path');
-
+var firebase = require('firebase');
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/pages/login/login.html'));
 });
 
-app.get('/pepe', function(req, res){
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/ventas', function(req, res){
+    res.sendFile(path.join(__dirname + '/pages/ventas/ventas.html'));
+});
+
+app.get('/productos', function(req, res){
+    res.sendFile(path.join(__dirname + 'pages/productos/productos.html'));
 });
 
 app.listen(3000, function(){
     console.log('Se abrio el servidor en el puerto 3000');
 });
 
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+  
+  var config = {
+    apiKey: "AIzaSyCYfdRAKVO9REjsR3fmupP4gGjFfxijlFw",
+    authDomain: "kioscogenesis-26031.firebaseapp.com",
+    databaseURL: "https://kioscogenesis-26031.firebaseio.com",
+    projectId: "kioscogenesis-26031",
+    storageBucket: "kioscogenesis-26031.appspot.com",
+    messagingSenderId: "320492236095"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+ }
+
+ var database = firebase.database();
