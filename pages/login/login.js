8251821556e22f626de041a5240208d467b096ec
unknown <emanuelginer77@gmail.com>
2019-01-05 20:30:28 -0300

    login

---
+++ pages/login/login.js
@@ -0,0 +1,20 @@
+alert("rasdasd");
+function validar() {
+
+
+
+
+    
+
+    var usuario = document.getElementById("usuario").value;
+    var contraseña = document.getElementById("contraseña").value;
+
+    if (usuario === "pako" && contraseña === "programacion"){
+        
+        alert("Entraste") 
+    }
+
+    else {
+        alert("Ingresa un usuario o contraseña validos")
+    }
+}
