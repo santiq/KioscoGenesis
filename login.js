257e6de6c91f91f1fd9ca3df11962ecbd99e0127
Mati <Phenrril@la-MacBook-Air-de-Mati.local>
2019-01-05 17:09:26 -0300

    Updated

--- login.js
+++ login.js
@@ -1,12 +1,4 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <body>
-            <script>
-                
+<script>                
 
-
-            </script>
-        </body>
-    </head>
-</html>
+    
+</script>
