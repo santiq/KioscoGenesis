9d7fe9732e85255c600adc61d41d9d9bcf572099
Matias Heredia <r0bin24xp@gmail.com>
2019-01-06 14:54:01 -0300

    Cambios en el Servidor, Conexion a Base de Datos Realizada y Route

--- app.js
+++ app.js
@@ -1,39 +1,62 @@
 var express = require('express');
 var app = express();
 var path = require ('path');
-var firebase = require('firebase');
+const bodyParser = require('body-parser');
+var admin = require("firebase-admin");
+var serviceAccount = require("./kioscogenesis-26031-firebase-adminsdk-mjxw1-7ff25d47c3.json");
 
-app.get('/', function(req, res){
+
+// ACA ESPECIFICO LOS DIRECTORIO DE ARCHIVOS ESTATICOS
+app.use(express.static(__dirname + '/pages/login'));
+app.use(express.static(__dirname + '/pages/login'));
+app.use(express.static(__dirname + '/pages/login'));
+
+
+// ACA INICIO TODO LO RELACIONADO A FIREBASE Y TIRO UNA PRUEBA EN CONSOLA
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kioscogenesis-26031.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("/");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+// -----------------------    Fin Firebase
+
+//extended: false significa que parsea solo string (no archivos de imagenes por ejemplo)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// ROUTES
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/pages/login/login.html'));
 });
 
-app.get('/ventas', function(req, res){
+app.get('/ventas', (req, res) => {
     res.sendFile(path.join(__dirname + '/pages/ventas/ventas.html'));
 });
 
-app.get('/productos', function(req, res){
-    res.sendFile(path.join(__dirname + 'pages/productos/productos.html'));
+app.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/productos/productos.html'));
 });
+app.get('/firebase', (req, res) => {
+    
+    //ref.push(productos);
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.post('/ventas', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/ventas/ventas.html'));
+})
+
+// FIN ROUTES
 
 app.listen(3000, function(){
     console.log('Se abrio el servidor en el puerto 3000');
 });
 
 
-// Initialize Firebase
-// TODO: Replace with your project's customized code snippet
-  
-  var config = {
-    apiKey: "AIzaSyCYfdRAKVO9REjsR3fmupP4gGjFfxijlFw",
-    authDomain: "kioscogenesis-26031.firebaseapp.com",
-    databaseURL: "https://kioscogenesis-26031.firebaseio.com",
-    projectId: "kioscogenesis-26031",
-    storageBucket: "kioscogenesis-26031.appspot.com",
-    messagingSenderId: "320492236095"
-  };
 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
- }
 
- var database = firebase.database();
